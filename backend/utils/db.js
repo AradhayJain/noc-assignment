da81d6c780a726de59cde08a65ef5026a4ee68ee
Aradhay Jain <aradhayjain2006@gmail.com>
2025-06-02 23:07:34 +0530

    ommition

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2"
 
 export const db = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root',
-    password: 'your_password', 
+    host: 'database-1.crewcyuyu6kf.ap-south-1.rds.amazonaws.com', 
+    user: 'admin',
+    password: 'Aradhay1234', 
     database: 'auth_demo' 
 });
