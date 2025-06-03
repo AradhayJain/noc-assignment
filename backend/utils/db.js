7a5cba4b750f999bfc0783eb435af9fa93a1b026
Aradhay Jain <aradhayjain2006@gmail.com>
2025-06-03 17:49:53 +0530

    db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,11 +2,8 @@ import mysql from "mysql2"
 // import mysql from "mysql2/promise";
 
 export const db = mysql.createConnection({
-    // host: 'database-1.crewcyuyu6kf.ap-south-1.rds.amazonaws.com', 
-    // user: 'admin',
-    // password: 'Aradhay1234', 
-    host:"localhost",
-    user:"root",
-    password:"your_password",
+    host: 'database-1.crewcyuyu6kf.ap-south-1.rds.amazonaws.com', 
+    user: 'admin',
+    password: 'Aradhay1234', 
     database: 'auth_demo' 
 });
